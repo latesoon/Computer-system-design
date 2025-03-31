d08dc9bc0c7d27fc21cdb32e0bb60dea0d8f6b06
tracer-ics2017 <tracer@njuics.org>
2025-03-31 16:26:14 +0800

    >  compile 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:26:14 up  3:06,  1 user,  load average: 0.54, 0.23, 0.19 d98a58491540fde392d70f01fab17ca82ed2d4c

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,14 +6,15 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  TODO();
-
+  //TODO();
+  rtl_push(&(id_dest->val));
   print_asm_template1(push);
 }
 
 make_EHelper(pop) {
-  TODO();
-
+  //TODO();
+  rtl_pop(&t1);
+  operand_write(id_dest,&t1);
   print_asm_template1(pop);
 }
 
