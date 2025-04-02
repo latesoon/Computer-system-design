655c6b3326c45c61a82be07e732ed708760b4dd2
tracer-ics2017 <tracer@njuics.org>
2025-04-02 15:20:14 +0800

    >  compile 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:20:14 up 13:25,  1 user,  load average: 0.45, 0.58, 0.53 9ad1ff619d06f6f9b338878b7b7852a699e41300

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -42,7 +42,7 @@ static inline make_DopHelper(SI) {
   if(op->width == 4)
     op->simm = instr_fetch(eip,op->width);
   else{
-    op->simm = (uint8_t)(instr_fetch(eip,op->width));
+    op->simm = (instr_fetch(eip,op->width)) & 0xFF ;
   }
 
 
--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -12,7 +12,7 @@ make_EHelper(jcc) {
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   decoding.is_jmp = t2;
-  printf("%d %x",decoding.is_jmp, decoding.jmp_eip);
+  //printf("%d %x",decoding.is_jmp, decoding.jmp_eip);
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
 }
 
