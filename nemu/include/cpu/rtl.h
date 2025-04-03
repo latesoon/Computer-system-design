18f3bfded77a7ed154af02f72326781d985ce655
tracer-ics2017 <tracer@njuics.org>
2025-04-03 11:46:05 +0800

    >  compile 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:46:05 up 18:58,  1 user,  load average: 0.84, 0.35, 0.23 95ed7e202b093a368633e809983a18e140a71782

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -160,17 +160,22 @@ static inline void rtl_pop(rtlreg_t* dest) {
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  //TODO();
+  rtl_sltui(dest, src1, 1);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  //TODO();
+  rtl_xori(dest, src1, imm);
+  rtl_eq0(dest, dest);
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+  //TODO();
+  rtl_eq0(dest, src1);
+  rtl_eq0(dest, dest);
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
