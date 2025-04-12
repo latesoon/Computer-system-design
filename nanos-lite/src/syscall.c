019a119cbbf9aba6991b45e2891e9642d50ea246
tracer-ics2017 <tracer@njuics.org>
2025-04-13 05:00:43 +0800

    >  run 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:00:43 up  5:40,  1 user,  load average: 0.29, 0.19, 0.18 773803987c12878a4be6342370e508f95ebf495f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,11 +4,14 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-
+  
   switch (a[0]) {
     case SYS_none:
       SYSCALL_ARG1(r) = 1;
       break;
+    case SYS_exit:
+      _halt(SYSCALL_ARG2(r));
+      break; 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
