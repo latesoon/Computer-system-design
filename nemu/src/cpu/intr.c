5496bc49f9326a3bec3cc06935836872148c3de9
tracer-ics2017 <tracer@njuics.org>
2025-04-12 20:35:09 +0800

    >  compile 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:35:09 up  1:28,  1 user,  load average: 1.66, 1.34, 0.89 9198fb77acf6d4ae575f7c0d2585fccce4439d59

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,13 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  //TODO();
+  rtl_push(&cpu.eflags);
+  rtl_push(&cpu.cs);
+  rtl_push(&ret_addr);
+
+  decoding.jmp_eip = (vaddr_read(cpu.idtr.base + 8 * NO, 4) & 0xFFFF)|(vaddr_read(cpu.idtr.base + 4 + 8 * NO, 4) & 0xFFFF0000);
+  decoding.is_jmp = 1;
 }
 
 void dev_raise_intr() {
