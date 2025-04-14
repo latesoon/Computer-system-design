c4941619b5994ded528044b26e26942ae7b67930
2211290-Yao Zhiyan <2211290@mail.nankai.edu.cn>
2025-04-14 15:43:31 +0800

    gitlog fix

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,21 @@
 #include "common.h"
 #include "syscall.h"
 
+int do_syswrite(int fd,const void* buf,size_t len){
+  if(fd == 1 || fd == 2){
+    for(int i=0;i<len;i++)
+      _putc(((char*)(buf))[i]);
+  }
+  //printf("%d %d\n",fd,len);
+  return len;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
   
   switch (a[0]) {
     case SYS_none:
@@ -12,6 +24,11 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:
       _halt(SYSCALL_ARG2(r));
       break; 
+    case SYS_write:
+      Log("SYS_write!");
+      SYSCALL_ARG1(r) = do_syswrite(((int)(a[1])), ((char*)(a[2])),((size_t)(a[3])));
+      break;
+    case SYS_brk:
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
