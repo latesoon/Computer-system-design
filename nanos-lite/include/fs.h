e233918de4b7571c63efa0d394a5d976fe7c0770
tracer-ics2017 <tracer@njuics.org>
2025-04-15 22:02:06 +0800

    >  run 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:02:06 up  7:06,  1 user,  load average: 1.10, 0.52, 0.27 dcb534a5f9d4ef76e6e042b2610fa2aa94b963

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -9,5 +9,7 @@ int fs_open(const char* pathname, int flags, int mode);
 ssize_t fs_read(int fd, void* buf, size_t len);
 int fs_close(int fd);
 size_t fs_filesz(int fd);
+off_t fs_lseek(int fd, off_t offset, int whence);
+ssize_t fs_write(int fd, const void* buf, size_t len);
 
 #endif
