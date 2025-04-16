5b5f731e031743732d9549dd7e7dfffdc476ff44
tracer-ics2017 <tracer@njuics.org>
2025-04-16 09:55:40 +0800

    >  run 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:55:40 up 10:18,  1 user,  load average: 0.25, 0.16, 0.12 7e55d44df42a1fad5e42d88bc4e9376627f7b9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,12 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  strncpy(buf, dispinfo+offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  assert(!(offset%sizeof(uint32_t) | len%sizeof(uint32_t)));
+  _draw_rect((uint32_t *)buf, offset/sizeof(uint32_t)%_screen.width, offset/sizeof(uint32_t)/_screen.width, len/sizeof(uint32_t),1);
 }
 
 void init_device() {
