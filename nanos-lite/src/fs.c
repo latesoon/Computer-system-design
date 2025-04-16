41abffb894d976b1331864d397bf6aa5307e6462
tracer-ics2017 <tracer@njuics.org>
2025-04-16 16:38:29 +0800

    >  run 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:38:29 up 10:35,  1 user,  load average: 0.40, 2.21, 1.42 bca34addda5e5105f138b4cca2921e40463f45e7

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -122,13 +122,13 @@ ssize_t fs_write(int fd, const void* buf, size_t len){
       _putc(((char*)(buf))[i]);
       break;
     case FD_FB:
-      fb_write(buf, tot_offset(fd), len);
+      fb_write(buf, fs_offset(fd), len);
       break;
     default:
       len = (fs_filesz(fd) - fs_offset(fd) >= len) ? len : (fs_filesz(fd) - fs_offset(fd));
       if(len <= 0) 
         return 0;
-      ramdisk_write(buf, fs_offset(fd), len);
+      ramdisk_write(buf, tot_offset(fd), len);
       update_offset(fd, len);
   }
   //Log(" newoffset %d\n",fs_offset(fd));
