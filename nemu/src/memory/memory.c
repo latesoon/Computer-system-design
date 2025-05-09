5a60abb9bc19b69a62793a7f512d05d24568add9
tracer-ics2017 <tracer@njuics.org>
2025-05-10 00:40:19 +0800

    >  compile 2211290 latesoon Linux latesoon-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:40:19 up 18:10,  1 user,  load average: 0.63, 0.47, 0.31 d4df44ca016f724ff7d2ea3f261846d63a6638c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -40,7 +40,7 @@ paddr_t page_translate(vaddr_t addr, bool write){
   //Log("CR3:%x BEGIN:%x ADDR:%x PDX:%x",cpu.cr3,BEGIN(cpu.cr3),addr,PDX(addr));
   PDE pde = (PDE)(paddr_read(BEGIN(cpu.cr3) + PDX(addr),4));
   //Log("PDE:%x",pde.val);
-  Assert(pde.present,"CR3:%x BEGIN:%x ADDR:%x PDX:%x",cpu.cr3,BEGIN(cpu.cr3),addr,PDX(addr));
+  Assert(pde.present,"CR3:%x BEGIN:%x ADDR:%x PDX:%x PDE:%x",cpu.cr3,BEGIN(cpu.cr3),addr,PDX(addr),pde.val);
   assert(pde.present);
 
   //Log("PDE:%x BEGIN:%x ADDR:%x PTX:%x",pde.val,BEGIN(pde.val),addr,PTX(addr));
